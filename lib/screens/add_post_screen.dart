1c424f22a55c6f24ebbe272e4daa28ed74fda79d
kemboy-svg <eliaskemboy645@.com>
2023-05-22 23:51:07 +0300

    adding changes

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -38,7 +38,54 @@ class _AddPostScreenState extends State<AddPostScreen> {
         ),
         )
       ],
-    )
+    ),
+    body:Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CircleAvatar(
+              backgroundImage: NetworkImage(
+                "https://images.unsplash.com/photo-1684158100445-8e6926faf964?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=401&q=80"
+              ),
+            ),
+            SizedBox(
+              
+              width: MediaQuery.of(context).size.width*0.4,
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Write your caption',
+                  border: InputBorder.none,
+                  
+                ),
+                maxLines: 8,
+              ),
+            ),
+            SizedBox(
+              width: 45,
+              height: 45,
+              child: AspectRatio(
+                aspectRatio:487/451,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage("https://images.unsplash.com/photo-1684158100445-8e6926faf964?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=401&q=80"),
+                      fit: BoxFit.fill,
+                      alignment: FractionalOffset.topCenter,
+                      ),
+
+                  ),
+                ),
+                 ),
+
+            ),
+          ],
+        ),
+
+      ],
+    
+    ),
     );
   }
 }
